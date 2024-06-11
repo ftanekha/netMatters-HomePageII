dd305d2ab5ec233860137fb5ac3f94248411a879
ftanekha <farai.tanekha@gmail.com>
2024-06-11 20:09:38 +0100

    Banner Slider - style the carousel buttons (edit slick-theme.css file) ~ 30mins - make all external files local - background-images failing to add dynamically,   can't locate code responsible in imported files   temporary fix: use inline style attr instead of mixin ~ 30mins
    - create a function to vertically center-align all banner text content wrappers @media >= 992px ~ 15mins

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(()=>{
+    //container for carousel
     $('div#banner').slick(
-        {
+        {//configuration object
             autoplay: true,
             arrows: false,
             dots: true
