a6bf71d5197031b0e6b4ff9a9ccd158621d7da90
ftanekha <farai.tanekha@gmail.com>
2024-08-15 14:31:59 +0100

    Created db table (news_items) ~~~~~~~~~~~~~~~~~~~~~~~ COLLATION=UTF-8_CI (Unicode Transformation Format – 8-bit) id: primary key, auto-increment category: varchar, max=20 posted-by: varchar , max=20, nullable, default=netmatters date: date, nullable, default=current-timestamp heading: unique, varchar, max=70 lead-paragraph: unique, varchar, max=160
    ISSUE: considerations when choosing btwn [varchar: selected] & text
    - the lengths of all headings & leading paragraphs must have a limit
    - the longest heading on the page has 58 characters, so heading: varchar(~70)
    - the longest paragraph on the page has 148 characters, so paragraph: varchar(~160)
    
    External resources used:
    https://www.lettercount.com/
    https://www.basedash.com/blog/mysql-text-vs-varchar
    https://unstop.com/blog/difference-between-char-and-vachar
    
    - Loop over these to display on your homepage
    - currently working on code for querying database tabel (news_items)
    
    Established connection with mysql database & Querying database tabel (news_items) using the PDO extension
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
    successfully:
    - instantiated connection to database i.e. $conn = new PDO(...);
    - queried database table: $result = $conn->query($query);
    - echo'd column data using foreach()
    - terminated connection: $db = null;
    
    External resources used:
    https://stackoverflow.com/questions/14104429/selecting-table-data-with-pdo-statements
    
    now moving onto to looping over news-items to display on homepage

--- includes/news-item.php
+++ includes/news-item.php
@@ -1,23 +1,20 @@
 <div class='latest-news-item md-screen-hidden'>
-    <a class='latest-news-item-category news_bespoke_software news_telecoms-services' href='#'>News</a>
+    <a class='latest-news-item-category news_bespoke_software news_telecoms-services' href='#'>
+    <?= $newsItemCategory ?>
+    </a>
     <img class='latest-news-photo' src='assets/images/news/east-anglian.png' alt='East Anglian air ambulancr' title='East Anglian air ambulance'>
     <div class='news-item-body'>
         <h3 class='havelockBlue'>
-            <a href='#'>
-                <?= $news-item-heading ?>
-            </a>
+            <a href='#'> <?= $newsItemHeading ?> </a>
         </h3>
-        <p>
-            The Championing &#8216;Your Local Life-Saving Charity&#8216; the East Anglian Air Ambulance 
-            (EAAA) is a renowned and brilliant charity providing life-saving critical care 24 hours ...
-        </p>
+        <p> <?= $newsItemLeadingParagraph ?> </p>
         <button class='havelockBlue'>READ MORE</button>
         <hr>
         <footer class='latest-news-footer'>
             <img src='assets/images/news/netmatters-mini-logo.webp' alt='netmatters logo'>
-            <strong>Posted by Netmatters</strong>
+            <strong>Posted by <?= $newsItemPostedBy ?>...</strong>
             <br>
-            <small>13th March 2024</small>
+            <small><?= $newsItemPostDate ?></small>
         </footer>
     </div>
 </div>
