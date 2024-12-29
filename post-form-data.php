e53194ffe954252afc11790c4ae2065f67c31997
ftanekha <farai.tanekha@gmail.com>
2024-12-29 01:50:13 +0000

    - remove redundant files - incorporate Qoddi env variables - add error report

--- post-form-data.php
+++ post-form-data.php
@@ -1,19 +1,21 @@
 <?php
-
-include 'loadenv.php';
-
-#get form data
-$host = "srv-captain--rbxhpafwje-mysql-80x";
-$dbname = getenv("MYSQL_DATABASE");
-$username = getenv("MYSQL_USER");
-$password = getenv("MYSQL_PASSWORD");
+error_reporting(-1);//report all errors
+ini_set("display_errors", "1");//shows all errors
+ini_set("log_errors", 1);
+ini_set("error_log", "/tmp/php-error.log");
+#get env variables
+$host = getenv("DATABASE_HOST");
+$dbname = getenv("DATABASE_NAME");
+$username = getenv("DATABASE_USERNAME");
+$password = getenv("DATABASE_PASSWORD");
+$dbPort = getenv("DATABASE_PORT");
+$dsn = "mysql:host=$host;dbname=$dbname;port:$dbPort";
 #instantiate connection to database
 try
 {
-    $conn = new PDO(
-        "mysql:host=$host;dbname=$dbname;", 
-        $username, $password
-    );
+    $conn = new PDO($dsn, $username, $password);
+    #throw any exception raised by PDO
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } 
 catch(PDOException $pe) 
 {
